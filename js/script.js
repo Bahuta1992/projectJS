bce0ddab3427ff6bbfc440b2be73d7b7acd210d3
Igor <kriked123@gmail.com>
2023-04-03 15:27:41 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+/* alert('hello') */
+
+
+/* const result = confirm('Are you here7'); */
+
+/* const answer = prompt("Вым есть 18?", "18");
+console.log(typeof(answer)); */
+
+/* const answers = [];
+
+answers[0] = prompt('КАКваше имя?', '');
+answers[1] = prompt('КАКваше фамилмя?', '');
+answers[2] = prompt('сколько вам лет?', '');
+
+console.log(typeof(answers)); */
+
+/* const category = 'toys'; */
+
+console.log('arr' + " - obj");
+
+
+
+
+
+
+
+
+/* /* let number = 4.6; /* числа */ 
+
+/* const persone = "Igor";  *//* строки */
+
+/* const bool = true/false; */ /* boolean */
+
+/* const obj = {  /* объкеты */
+/*    name: "John",
+    age: 25,
+    isMarriad: false */
+/* };  */ 
+
+/* console. log(obj.name);
+
+
+let arr = ['plum.png', 'orange.jpg', {},];
+console. log(arr[1]); */
+
+
+/* const arr = ['a', 'b', 'c']; */ /* массив */
+/* const arrObj = {
+    a: 'a',
+    b: 'b',
+    c: 'c'
+}; */
+/* arrObj.b = 
+console. log(arrObj [c]);
+const obj = {a: 1, b: 2}; */ /* объкеты */
+
+/* const obj = {
+    'Anna':500,
+    'Alice':800
+}; */ 
+
+
+
