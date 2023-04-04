af541a56688d4abe13d1615418aeeb62ec94e11e
Igor <kriked123@gmail.com>
2023-04-04 17:36:03 +0300

    first practic

--- js/script.js
+++ js/script.js
@@ -1,65 +1,20 @@
 "use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-/* alert('hello') */
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-/* const result = confirm('Are you here7'); */
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-/* const answer = prompt("Вым есть 18?", "18");
-console.log(typeof(answer)); */
-
-/* const answers = [];
-
-answers[0] = prompt('КАКваше имя?', '');
-answers[1] = prompt('КАКваше фамилмя?', '');
-answers[2] = prompt('сколько вам лет?', '');
-
-console.log(typeof(answers)); */
-
-/* const category = 'toys'; */
-
-console.log('arr' + " - obj");
-
-
-
-
-
-
-
-
-/* /* let number = 4.6; /* числа */ 
-
-/* const persone = "Igor";  *//* строки */
-
-/* const bool = true/false; */ /* boolean */
-
-/* const obj = {  /* объкеты */
-/*    name: "John",
-    age: 25,
-    isMarriad: false */
-/* };  */ 
-
-/* console. log(obj.name);
-
-
-let arr = ['plum.png', 'orange.jpg', {},];
-console. log(arr[1]); */
-
-
-/* const arr = ['a', 'b', 'c']; */ /* массив */
-/* const arrObj = {
-    a: 'a',
-    b: 'b',
-    c: 'c'
-}; */
-/* arrObj.b = 
-console. log(arrObj [c]);
-const obj = {a: 1, b: 2}; */ /* объкеты */
-
-/* const obj = {
-    'Anna':500,
-    'Alice':800
-}; */ 
-
-
-
+console.log(personalMovieDB);
