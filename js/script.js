d3adbbe0c0ee5abde75b514d011525e14e11678f
Igor <kriked123@gmail.com>
2023-04-05 17:34:23 +0300

    first practic2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 "use strict";
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+/* const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -17,4 +17,110 @@ const a = prompt('Один из последних просмотренных ф
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+console.log(personalMovieDB); */
+
+
+// eslint-disable-next-line no-constant-condition
+/* if (4 == 4) {
+	console.log("ok!");
+} else{
+	console.log("error!");
+}
+
+const num = 50;
+if( num < 49) {
+	console.log ("error");
+} else if (num > 100){
+	console.log ("много!");
+} else{
+	console.log ("ok");
+}
+
+(num === 50) ? console.log("ok") : console.log("error"); */
+
+/* const hamburger = 5;
+const fries = 0;
+
+if (hamburger && fries) {
+    console.log("я сыт");
+} */
+
+/* const hamburger = 3;
+const fries = 1;
+const cola = 1;
+
+console.log(hamburger === 3 && cola  && fries);
+
+console.log(1 && 0);
+console.log(1 && 5);
+console.log(null && 5);
+console.log(0 && "dfdfdsf");
+
+if (hamburger === 3 && cola === 1 && fries) {
+    console.log("все сыты");
+} else {
+    console.log("мы уходим");
+} */
+/* const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+    console.log("все сыты");
+} else {
+    console.log("мы уходим");
+}
+console.log(hamburger || cola || fries);
+
+let johnReport, alexReport, samReport, mariaReport = "done";
+console.log(johnReport || alexReport || samReport || mariaReport);
+
+
+console.log(!1); */
+
+/* console.log( NaN || 2 || undefined );
+console.log( NaN && 2 && undefined );
+console.log( 1 && 2 && 3 );
+console.log( !1 && 2 || !3 );
+console.log( 25 || null && !3 );
+console.log( NaN || null || !3 || undefined || 5);/*  или на правильном
+ */
+
+/* console.log( 1 || 5);
+
+
+console.log( 5 === 5 && 3 > 1 || 5);
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!')
+}
+
+
+let hamburger;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+ */ 
